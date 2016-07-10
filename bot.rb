786248f2f6a5379dc49a35247d74899c5d483414
katez <katiezechar@yahoo.com>
2016-07-10 18:56:06 +0200

    update text used in reminder posts

--- bot.rb
+++ bot.rb
@@ -18,7 +18,7 @@ client = Slack::RealTime::Client.new
 
 def create_slack_post client
   # client.chat_postMessage(channel: '#random', text: 'Hello World', as_user: false)
-  client.files_upload(
+  @last_post=client.files_upload(
     channels: '#random',
      as_user: true,
      content: "#{week_from_today} SHS
@@ -33,12 +33,26 @@ def create_slack_post client
   )
 end
 
+def create_wednesday_reminder client
+  client.chat_postMessage(channel: '#random', text: "<!channel>: Please contribute to <#{last_post_url}|SHS agenda>", as_user: true)
+end
+
+def create_friday_reminder client
+  client.chat_postMessage(channel: '#random', text: "<!channel>: Learn anything new this week? Add it to the <#{last_post_url}|SHS agenda>", as_user: true)
+end
+
 def week_from_today
   Date.today+7
+end
+
+def last_post_url
+  @last_post&.file&.url_private
 end
 # listen for hello (connection) event - https://api.slack.com/events/hello
 client.on :hello do
   logger.debug("Connected '#{client.self['name']}' to '#{client.team['name']}' team at https://#{client.team['domain']}.slack.com.")
 end
 
-Clockwork.every(1.week, 'post.shs_agenda', at: 'Tuesday 12:00', tz: 'Europe/Berlin') { create_slack_post Slack::Web::Client.new }
+Clockwork.every(1.week, 'post.shs_agenda', at: 'Tuesday 13:00', tz: 'Europe/Berlin') { create_slack_post Slack::Web::Client.new }
+Clockwork.every(1.week, 'post.reminder', at: 'Wednesday 16:00', tz: 'Europe/Berlin') { create_wednesday_reminder Slack::Web::Client.new }
+Clockwork.every(1.week, 'post.reminder', at: 'Friday 16:00', tz: 'Europe/Berlin') { create_friday_reminder Slack::Web::Client.new }
