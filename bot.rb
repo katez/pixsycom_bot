86f9bd77a717099844af0556a50da65e97fd0095
katez <katiezechar@yahoo.com>
2016-07-19 23:56:23 +0200

    create workflow for toggl project

--- bot.rb
+++ bot.rb
@@ -4,7 +4,10 @@ require 'clockwork'
 require 'date'
 
 CHANNEL = '#botty_goes_for_a_sail'
+USER_TOGGL_TO_SLACK_MAPPING = {
+  2071434 => 'kt'
 
+}
 logger = Logging.logger(STDOUT)
 logger.level = :debug
 
@@ -35,6 +38,10 @@ def create_slack_post client
   )
 end
 
+def create_direct_message_nudge client, user
+  client.chat_postMessage(channel: user, text: "It looks like you haven't logged your time in the last 30 minutes", as_user: true)
+end
+
 def create_wednesday_reminder client
   client.chat_postMessage(channel: CHANNEL, text: "<!channel>: Please contribute to <#{last_post_url}|SHS agenda>", as_user: true)
 end
@@ -45,6 +52,13 @@ end
 
 def week_from_today
   Date.today+7
+end
+
+def nudge_user_toggl_time client
+  # get all users from toggl
+  # filter out irrelevant users in management roles
+  # filter the users down to who hasn't logged in recently
+  # message these users  
 end
 
 def last_post_url
@@ -58,3 +72,5 @@ end
 Clockwork.every(1.week, 'post.shs_agenda', at: 'Tuesday 13:00', tz: 'Europe/Berlin') { create_slack_post Slack::Web::Client.new }
 Clockwork.every(1.week, 'post.reminder', at: 'Wednesday 16:00', tz: 'Europe/Berlin') { create_wednesday_reminder Slack::Web::Client.new }
 Clockwork.every(1.week, 'post.reminder', at: 'Friday 16:00', tz: 'Europe/Berlin') { create_friday_reminder Slack::Web::Client.new }
+
+Clockwork.every(3.minutes, 'post.message.nudge') { nudge_user_toggl_time Slack::Web::Client.new }
